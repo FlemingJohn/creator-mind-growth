e70264dcb995383759d71586d8be0c0d12b3aa4d
FlemingJohn <flemjohn08@gmail.com>
2026-08-27 23:26:14 +0530

    add stop words

---
+++ lib/asks/stopWords.ts
@@ -0,0 +1,14 @@
+export const stopWords = new Set<string>([
+  "the", "and", "for", "you", "your", "this", "that", "with", "have", "has", "was", "were",
+  "are", "but", "not", "can", "could", "would", "should", "will", "just", "like", "what",
+  "when", "how", "why", "who", "which", "from", "into", "about", "more", "most", "some",
+  "any", "all", "out", "get", "got", "one", "two", "its", "his", "her", "their", "them",
+  "they", "there", "here", "then", "than", "very", "much", "really", "please", "thanks",
+  "thank", "video", "videos", "channel", "watch", "watching", "watched", "make", "made",
+  "making", "want", "wanted", "need", "needs", "see", "seen", "look", "looks", "good",
+  "great", "nice", "love", "loved", "best", "also", "still", "even", "back", "over",
+  "after", "before", "next", "last", "first", "new", "old", "does", "did", "doing",
+  "was", "been", "being", "were", "our", "ours", "yours", "mine", "him", "she", "he",
+  "it", "is", "in", "on", "of", "to", "a", "an", "as", "at", "by", "be", "or", "if",
+  "so", "do", "no", "yes", "up", "we", "me", "my", "i", "u", "ur", "pls", "plz"
+]);
